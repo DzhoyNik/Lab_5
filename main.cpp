117f5a1dd2abb2dcac93058b81bbd69388a01d86
DzhoyNik <57442310+DzhoyNik@users.noreply.github.com>
2024-10-16 02:14:23 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+void summElement(int& sum, int& sumSquart) {
+	srand(time(0));
+	const int n = 10;
+	int R[n];
+	sum = 0;
+	sumSquart = 0;
+
+	for (size_t i = 0; i < n; i++) {
+		R[i] = rand() % 21 - 10;
+		cout << R[i] << " ";
+	}
+
+	for (size_t i = 0; i < n; i++) {
+		sum += R[i];
+
+		if (R[i] >= 0) {
+			sumSquart += sqrt(R[i]);
+		}
+	}
+
+	cout << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	int sum;
+	int sumSquart;
+
+	summElement(sum, sumSquart);
+
+	cout << "Сумма элементов: " << sum << endl << "Сумма квадратных корней: " << sumSquart;
+}
